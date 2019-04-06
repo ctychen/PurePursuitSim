a527c4e41531fe8996866b2c54e4ee26a6e3bdf7
C T Chen <34198336+the-null-terminator@users.noreply.github.com>
2019-04-06 16:10:39 -0700

    Create CSV.java

---
+++ src/CSV.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * Stores basic stuff about CSV to make life easier
+ */
+public class CSV {
+	public String name;
+	public ArrayList<Object> log;
+
+	public CSV(String name, ArrayList<Object> log) {
+		this.name = name;
+		this.log = log;
+	}
+
+}
